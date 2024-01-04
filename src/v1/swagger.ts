cbdd1f73c2e773d215e05c60d77389e48b3a9e89
Nacho <nachogassie@gmail.com>
2024-01-04 01:36:45 -0300

    swagger fix

--- src/v1/swagger.ts
+++ src/v1/swagger.ts
@@ -1,5 +1,6 @@
 import { join } from "path";
 import swaggerJsDoc from "swagger-jsdoc";
+import { PORT } from "../config";
 
 const options = {
     definition: {
@@ -10,7 +11,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3040"
+                url: `http://localhost:${PORT}`
             }
         ]
     },
